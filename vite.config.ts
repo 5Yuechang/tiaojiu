782d72c9890c52548b0547de7c98a6988aec63c8
5Yuechang <774034048@qq.com>
2024-12-22 18:18:53 -0800

    update: 更新 base 配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/V4-11-Hall-A/',
+    base: '/tiaojiu/',  // 改为新仓库名
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
