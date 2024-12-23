a0d2aadecff5f8a7229e2aba7f98f77af86063ef
5Yuechang <774034048@qq.com>
2024-12-22 18:30:55 -0800

    fix: 统一配置文件，更新部署配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: './',  // 改为相对路径
+    base: '/tiaojiu/',  // 改回仓库名
     build: {
         outDir: 'dist',
         rollupOptions: {
